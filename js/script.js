1afb9bcbfd827bbd2a982f2d1929e2ba2771147a
MarySanchezC <marysanchezc1204@gmail.com>
2021-12-05 22:55:11 -0500

    mean

---
+++ js/script.js
@@ -0,0 +1,11 @@
+const login1 = document.getElementById("login1");
+const login2 = document.getElementById("login2");
+const loginUser = document.getElementById("loginUser");
+
+const textLogin = document.getElementById("textLogin");
+const textLogout = document.getElementById("textLogout");
+
+
+
+
+
